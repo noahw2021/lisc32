6a142b05c25df6346befc03710560ebd1993cf9a
Noah Wooten <noah.wooten@icloud.com>
2024-11-25 14:16:34 -0800

    rename files

--- lisc32_isn/isn_parse.c
+++ lisc32_isn/isn_parse.c
@@ -6,62 +6,120 @@
 //
 
 #include <stdio.h>
+#include <string.h>
 #include "isn.h"
-#include "types.h"
 
-int IsnInstructionCount(void* _Ctx) {
-    PSIN_CTX Ctx = _Ctx;
-    return Ctx->InstructionCount;
-}
-
-char* IsnGetInstructionName(void* _Ctx, int Id) {
-    PSIN_CTX Ctx = _Ctx;
-    return Ctx->Instructions[Id].Instruction;
-}
-
-BYTE IsnGetInstructinOpcode(void* _Ctx, int Id) {
-    PSIN_CTX Ctx = _Ctx;
-    return Ctx->Instructions[Id].Opcode;
-}
-
-BYTE IsnGetOperandType(void* _Ctx, int Id, BYTE OperandId) {
-    PSIN_CTX Ctx = _Ctx;
-    return Ctx->Instructions[Id].Operands[OperandId].Type;
-}
-
-BYTE IsnGetPhysicalSize(void* _Ctx, int Id, BYTE OperandId) {
-    PSIN_CTX Ctx = _Ctx;
-    return Ctx->Instructions[Id].Operands[OperandId].PhysicalSize;
-}
-
-BYTE IsnGetUsableSize(void* _Ctx, int Id, BYTE OperandId) {
-    PSIN_CTX Ctx = _Ctx;
-    return Ctx->Instructions[Id].Operands[OperandId].VirtualSize;
-}
-
-char* IsnGetOperandName(void* _Ctx, int Id, BYTE OperandId) {
-    PSIN_CTX Ctx = _Ctx;
-    return Ctx->Instructions[Id].Operands[OperandId].OperandNames;
-}
-
-BYTE IsnGetTotalSize(void* _Ctx, int Id) {
-    PSIN_CTX Ctx = _Ctx;
-    return Ctx->Instructions[Id].InstructionSize;
-}
-
-char* IsnGetDescription(void* _Ctx, int Id) {
-    PSIN_CTX Ctx = _Ctx;
-    return Ctx->Instructions[Id].InstructionDescription;
-}
-
-int IsnGetOperandCount(void* Ctx, int Id) {
+void* IsnInit(void) {
+    PISN_CTX Ctx;
+    Ctx = malloc(sizeof(ISN_CTX));
+    memset(Ctx, 0, sizeof(ISN_CTX));
     
+    return Ctx;
+}
+void IsnShutdown(void* Ctx) {
+    free(Ctx->Instructions);
+    free(Ctx);
+    
+    return;
 }
 
-int  IsnGetInstructionByName(void* Ctx, const char* Operand) {
+void IsnLoadData(void* _Ctx, char* _Data) {
+    PSIN_CTX Ctx = _Ctx;
+    if (Data[0] == '/')
+        return;
     
-}
-
-int  IsnGetInstructionByOpcode(void* Ctx, BYTE Opcode) {
+    if (!Ctx->Instructions) {
+        Ctx->Instructions = malloc(sizeof(ISN_INSTRUCTION));
+    } else {
+        Ctx->Instructions = realloc(Ctx->Instructions,
+            sizeof(ISN_INSTRUCTION) * (Ctx->InstructionCount + 1));
+    }
     
+    PISN_INSTRUCTION NewInst = &Ctx->Instructions[Ctx->InstructionCount++];
+    char* Data = _Data;
+    
+    // __LDI 00-80 (R:04,08=__DEST) (I:64,64=__IMMD) : Loads Immediate to Registers
+    
+    // Parse Instruction Mnemonic
+    for (int i = 0; i < 5; i++) {
+        NewInst->Instruction[i] = *Data;
+        Data++;
+    }
+    
+    // Parse Opcode
+    NewInst->Opcode = strtoul(Data, NULL, 16);
+    Data += 3;
+    
+    // Parse Instruction Size
+    NewInst->InstructionSize = strtoull(Data, NULL, 10);
+    Data += 3;
+    
+    // Parse Operand 1 (if Present)
+    if (*Data == '(') {
+        // Operand Present
+        Data++;
+        
+        // Parse Type
+        if (*Data == 'R')
+            NewInst->Operands[0] = 0;
+        else
+            NewInst->Operands[0] = 1;
+        Data += 2;
+        
+        // Parse Virtual Size
+        NewInst->Operands[0].VirtualSize = strtoull(Data, NULL, 10);
+        Data += 3;
+        
+        // Parse Physical Size
+        NewInst->Operands[0].PhysicalSize = strtoull(Data, NULL, 10);
+        Data += 3;
+        
+        // Parse Operand Name
+        for (int i = 0; i < 6; i++) {
+            NewInst->Operands[0].OperandNames[i] = *Data;
+            Data++;
+        }
+        
+        // Account for ') '
+        Data += 2;
+    }
+    
+    // Parse Operand 2 (if Present)
+    if (*Data == '(') {
+        // Operand Present
+        Data++;
+        
+        // Parse Type
+        if (*Data == 'R')
+            NewInst->Operands[1] = 0;
+        else
+            NewInst->Operands[1] = 1;
+        Data += 2;
+        
+        // Parse Virtual Size
+        NewInst->Operands[1].VirtualSize = strtoull(Data, NULL, 10);
+        Data += 3;
+        
+        // Parse Physical Size
+        NewInst->Operands[1].PhysicalSize = strtoull(Data, NULL, 10);
+        Data += 3;
+        
+        // Parse Operand Name
+        for (int i = 0; i < 6; i++) {
+            NewInst->Operands[1].OperandNames[i] = *Data;
+            Data++;
+        }
+        
+        // Account for ') '
+        Data += 2;
+    }
+    
+    // Parse Instruction Description
+    while (*Data != ':')
+        Data++;
+    Data++;
+    
+    strlcpy(NewInst->InstructionDescription, 256, Data);
+    
+    return;
 }
