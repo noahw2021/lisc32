35112d97ef40b137f7e8f3f89475e5cb65d28747
Noah Wooten <noah.wooten@icloud.com>
2024-11-25 10:48:11 -0800

    create lisc32_isn project 2

--- lisc32_emu/cpu/cpu.h
+++ lisc32_emu/cpu/cpu.h
@@ -16,27 +16,27 @@ extern void(*Instructions[256])(void);
 enum {
     // General Instructions
     
-    ____LDI = 0x00, // __LDI 00-80 (R:04,08=__DEST) (I:64,64=__IMMD)
-    ____JMP = 0x01, // __JMP 01-16 (R:04,08=___REG)
-    ___CALL = 0x02, // _CALL 02-16 (R:04,08=___REG)
-    ____RET = 0x03, // __RET 03-08
-    ____CMP = 0x04, // __CMP 04-16 (R:04,04=__CMP1) (R:04,04=__CMP2)
-    ____MOV = 0x05, // __MOV 05-16 (R:04,04=__DEST) (R:04,04=___SRC)
-    __NXCLR = 0x06, // NXCLR 06-08
-    __NXEQL = 0x07, // NXEQL 07
-    __NXZRO = 0x08, // NXZRO 08
-    __NXGRT = 0x09, // NXGRT 09
-    __NXLST = 0x0A, // NXLST 0A
+    ____LDI = 0x00, // __LDI 00-80 (R:04,08=__DEST) (I:64,64=__IMMD) : Loads Immediate to Registers
+    ____JMP = 0x01, // __JMP 01-16 (R:04,08=___REG) : Jumps to Register
+    ___CALL = 0x02, // _CALL 02-16 (R:04,08=___REG) : Calls to Register
+    ____RET = 0x03, // __RET 03-08 : Returns
+    ____CMP = 0x04, // __CMP 04-16 (R:04,04=__CMP1) (R:04,04=__CMP2) : Compares two registers
+    ____MOV = 0x05, // __MOV 05-16 (R:04,04=__DEST) (R:04,04=___SRC) : Moves register to another register
+    __NXCLR = 0x06, // NXCLR 06-08 : NextExecute Clear
+    __NXEQL = 0x07, // NXEQL 07-08 : NextExecute If Equal
+    __NXZRO = 0x08, // NXZRO 08-08 : NextExecute If Zero
+    __NXGRT = 0x09, // NXGRT 09-08 : NextExecute If Greater
+    __NXLST = 0x0A, // NXLST 0A-08 : NextExecute If LessThan
     
     // Arithmetic & Bitwise
-    ____ADD = 0x20, // __ADD 20
-    ____SUB = 0x21, // __SUB 21
-    ____MUL = 0x22, // __MUL 22
-    ____DIV = 0x23, // __DIV 23
-    ____MOD = 0x24, // __MOD 24
-    ____AND = 0x25, // __AND 25
-    _____OR = 0x26, // ___OR 26
-    ____XOR = 0x27, // __XOR 27
+    ____ADD = 0x20, // __ADD 20-16 (R:04,04=__DEST) (R:04,04=___ARG) : Adds to Destination
+    ____SUB = 0x21, // __SUB 21-16 (R:04,04=__DEST) (R:04,04=___ARG) : Subtracts from Destination
+    ____MUL = 0x22, // __MUL 22-16 (R:04,04=__DEST) (R:04,04=___ARG) : Multiplies Destination
+    ____DIV = 0x23, // __DIV 23-16 (R:04,04=__DEST) (R:04,04=___ARG) : Divides Destination by Argument, Quotient stored
+    ____MOD = 0x24, // __MOD 24-16 (R:04,04=__DEST) (R:04,04=___ARG) : Divides Destination by Argument, Remainder stored
+    ____AND = 0x25, // __AND 25-16 (R:04,04=__DEST) (R:04,04=___ARG) : Bitwise AND Destiation by Argument
+    _____OR = 0x26, // ___OR 26-16 (R:04,04=__DEST) (R:04,04=___ARG) : Bitwise OR Destination by Argument
+    ____XOR = 0x27, // __XOR 27-16 (R:04,04=__DEST) (R:04,04=___ARG) : Bitwise XOR Destination by Argument
 };
 
 #endif /* cpu_h */
