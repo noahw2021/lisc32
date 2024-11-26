68d8ce15d89815203e450310fefa6826edb60a96
Noah Wooten <noah.wooten@icloud.com>
2024-11-26 09:06:51 -0800

    add linker to compiler

--- lisc32_asm/codegen/codegen.h
+++ lisc32_asm/codegen/codegen.h
@@ -14,7 +14,17 @@
 typedef struct _CODEGEN_ERROR {
     int ErrorCode;
     int Line;
+    char Msg[256];
 }CODEGEN_ERROR, *PCODEGEN_ERROR;
+
+typedef struct _CODEGEN_SYMBOL {
+    WORD64 Resolution;
+    char SymbolName[256];
+    BYTE IsResolved;
+    
+    WORD64 LocationCount;
+    PWORD64 Locations;
+}CODEGEN_SYMBOL, *PCODEGEN_SYMBOL;
 
 typedef struct _CODEGEN_CTX {
     int LineCount, CurrentLine;
@@ -31,20 +41,31 @@ typedef struct _CODEGEN_CTX {
     
     int InFileCount, InFileMax;
     FILE** InFiles;
+    
+    WORD64 SymbolCount, SymbolMax;
+    PCODEGEN_SYMBOL Symbols;
 }CODEGEN_CTX, *PCODEGEN_CTX;
 extern PCODEGEN_CTX CgCtx;
 
+// codegen_init.c
 void CgInit(void);
 void CgShutdown(void);
 
+// codegen_parse.c
 void CgParseLine(char* Line);
+
+// codegen_compiler.c
 int CgGetErrorCount(void);
 void CgError(int Line, int Code, const char* Msg);
 void CgCompile(void);
+WORD64 CgLinkGetSymbol(char* SymbolName, int Offset);
+void CgLinkResolveSymbol(char* SymbolName, WORD64 Ptr);
 
+// codegen_files.c
 void CgAddFile(char* FileName);
 char* CgReadLine(char* Line, int Max);
 
+// codegen_data.c
 void CgPut1(BYTE Data);
 void CgPut2(WORD16 Data);
 void CgPut4(WORD32 Data);
--- lisc32_asm/codegen/codegen_compiler.c
+++ lisc32_asm/codegen/codegen_compiler.c
@@ -6,4 +6,31 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "codegen.h"
+
+int CgGetErrorCount(void) {
+    return CgCtx->ErrorCount;
+}
+
+void CgError(int Line, int Code, const char* Msg) {
+    if (CgCtx->ErrorCount == CgCtx->ErrorMax) {
+        CgCtx->ErrorMax += 256;
+        CgCtx->Errors = realloc(CgCtx->Errors,
+            sizeof(CODEGEN_ERROR) * CgCtx->ErrorMax);
+    }
+    
+    PCODEGEN_ERROR NewError = &CgCtx->Errors[CgCtx->ErrorCount];
+    CgCtx->ErrorCount++;
+    
+    NewError->ErrorCode = Code;
+    NewError->Line = Line;
+    strlcpy(NewError->Msg, Msg, 256);
+    
+    return;
+}
+
+void CgCompile(void) {
+    	
+}
