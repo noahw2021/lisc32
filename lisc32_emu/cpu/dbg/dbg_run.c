662a3fb0faaca02e1d9d9702e913db592645b527
Noah Wooten <noah.wooten@icloud.com>
2024-12-07 00:53:35 -0800

    more debugger work

--- lisc32_emu/cpu/dbg/dbg_run.c
+++ lisc32_emu/cpu/dbg/dbg_run.c
@@ -10,8 +10,25 @@
 #include "isn/isn_imports.h"
 
 void DbgRun(BYTE Opcode) {
+    if (!(GlobalFlags & FLAG_USEDEBUGGER))
+        return;
+    
     int IsnId = IsnGetInstructionByOpcode(IsnCtx, Opcode);
     // if (IsnId == -1) @TODO figure this out later 202412062151
     
+    char Operand1[32];
+    char Operand2[32];
     
+    if (IsnGetOperandCount(IsnCtx, IsnId) == 2) {
+        
+    } else if (IsnGetOperandCount(IsnCtx, IsnId) == 1) {
+        if (IsnGetOperandType(IsnCtx, IsnId, 0) == 0) { // Register
+            BYTE Register = DbgRead1();
+            printf("%s r%i\n", IsnGetInstructionName(IsnCtx, IsnId), Register);
+        } else { // Immediate
+            
+        }
+    } else { // 0 Operands
+        printf("%s\n", IsnGetInstructionName(IsnCtx, IsnId));
+    }
 }
--- lisc32_emu/main.c
+++ lisc32_emu/main.c
@@ -6,14 +6,31 @@
 //
 
 #include <stdio.h>
+#include <string.h>
 #include "types.h"
 #include "cpu/dbg/isn/isn_imports.h"
 
 WORD64 GlobalFlags;
 
 int main(int argc, char** argv) {
+    GlobalFlags = 0x00;
+    
     for (int i = 0; i < argc; i++) {
+        if (!strcmp(argv[i], "-h") || !strcmp(argv[i], "--help")) {
+            printf("%s: Help & Usage\n", argv[0]);
+            printf("%s (-h | --help) : Displays this message.\n", argv[0]);
+            printf("%s (-d | --debug) : Enables transcription of executed instructions.\n", argv[0]);
+            printf("%s (-sd | --stack-debug) : Enables reading of the immediate stack.\n", argv[0]);
+            
+        }
         
+        if (!strcmp(argv[i], "-d") || !strcmp(argv[i], "--debug")) {
+            GlobalFlags |= FLAG_USEDEBUGGER;
+        }
+        
+        if (!strcmp(argv[i], "-sd") || !strcmp(argv[i], "--stack-debug")) {
+            GlobalFlags |= FLAG_SHOWSTACK;
+        }
     }
     
     return 0;
