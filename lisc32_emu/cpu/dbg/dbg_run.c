5ee064f911010c955c9358700cb631b48e4b7026
Noah Wooten <noah.wooten@icloud.com>
2024-12-09 09:23:46 -0800

    finish debug parser

--- lisc32_emu/cpu/dbg/dbg_run.c
+++ lisc32_emu/cpu/dbg/dbg_run.c
@@ -20,15 +20,91 @@ void DbgRun(BYTE Opcode) {
     char Operand2[32];
     
     if (IsnGetOperandCount(IsnCtx, IsnId) == 2) {
+        printf("%s ", IsnGetInstructionName(IsnCtx, IsnId));
         
+        if (IsnGetOperandType(IsnCtx, IsnId, 0) == 0) { // Register
+            if (IsnGetPhysicalSize(IsnCtx, IsnId, 0) == 4) { // Two registers per byte
+                union {
+                    BYTE As8;
+                    struct {
+                        BYTE As4_0 : 4;
+                        BYTE As4_1 : 4;
+                    };
+                }DoubleByte;
+                
+                DoubleByte.As8 = DbgRead1();
+                printf("r%i, r%i\n", DoubleByte.As4_0, DoubleByte.As4_1);
+                return;
+            } else {
+                BYTE Operand1 = DbgRead1();
+                WORD64 Immediate;
+                switch (IsnGetPhysicalSize(IsnCtx, IsnId, 0))  {
+                    default:
+                    case 8:
+                        Immediate = DbgRead1();
+                        break;
+                    case 16:
+                        Immediate = DbgRead2();
+                        break;
+                    case 32:
+                        Immediate = DbgRead4();
+                        break;
+                    case 64:
+                        Immediate = DbgRead8();
+                        break;
+                };
+                
+                printf("r%i, %llXh", Operand1 & 0xF, Immediate);
+                return;
+                // Operand 2 must be an immediate
+            }
+        } else { // Immediate
+            WORD64 Immediate;
+            switch (IsnGetPhysicalSize(IsnCtx, IsnId, 0))  {
+                default:
+                case 8:
+                    Immediate = DbgRead1();
+                    break;
+                case 16:
+                    Immediate = DbgRead2();
+                    break;
+                case 32:
+                    Immediate = DbgRead4();
+                    break;
+                case 64:
+                    Immediate = DbgRead8();
+                    break;
+            };
+            
+            
+        }
     } else if (IsnGetOperandCount(IsnCtx, IsnId) == 1) {
         if (IsnGetOperandType(IsnCtx, IsnId, 0) == 0) { // Register
             BYTE Register = DbgRead1();
             printf("%s r%i\n", IsnGetInstructionName(IsnCtx, IsnId), Register);
         } else { // Immediate
+            WORD64 Immediate;
+            switch (IsnGetPhysicalSize(IsnCtx, IsnId, 0))  {
+                default:
+                case 8:
+                    Immediate = DbgRead1();
+                    break;
+                case 16:
+                    Immediate = DbgRead2();
+                    break;
+                case 32:
+                    Immediate = DbgRead4();
+                    break;
+                case 64:
+                    Immediate = DbgRead8();
+                    break;
+            };
             
+            printf("%s %llXh\n", IsnGetInstructionName(IsnCtx, IsnId), Immediate);
         }
     } else { // 0 Operands
         printf("%s\n", IsnGetInstructionName(IsnCtx, IsnId));
     }
+    
+    return;
 }
