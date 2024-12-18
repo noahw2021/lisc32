b1308d371468a1e91ce37b547aedc1fdbfb67774
Noah Wooten <noah.wooten@icloud.com>
2024-12-18 15:03:16 -0800

    implement memory

--- lisc32_emu/cpu/ops/ops_memory.c
+++ lisc32_emu/cpu/ops/ops_memory.c
@@ -45,13 +45,21 @@ void STMEM(void) {
 } // STMEM 41-16 (R:04,04=__DEST) (R:04,04=SOURCE) : Stores memory from register
 
 void _PUSH(void) {
+    BYTE As8 = MmuRead1();
+    As8 &= 0x0F;
+    MmuPush(Rs->GPRs[As8]);
     return;
 } // _PUSH 42-16 (R:04,08=SOURCE) : Pushes a register onto the stack
 
 void __POP(void) {
+    BYTE As8 = MmuRead1();
+    As8 &= 0xF;
+    Rs->GPRs[As8] = MmuPull();
     return;
 } // __POP 42-16 (R:04,08=__DEST) : Pops from the stack into a register
 
 void PUSHI(void) {
+    WORD32 Immd = MmuRead4();
+    MmuPush(Immd);
     return;
 } // PUSHI 42-40 (I:32,32=SOURCE) : Pushes an immediate to the stack
