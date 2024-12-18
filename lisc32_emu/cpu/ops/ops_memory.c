02f80751cf4259c5355cbfa70023a73223214988
Noah Wooten <noah.wooten@icloud.com>
2024-12-18 08:54:41 -0800

    implement memory instructions

--- lisc32_emu/cpu/ops/ops_memory.c
+++ lisc32_emu/cpu/ops/ops_memory.c
@@ -7,4 +7,39 @@
 
 #include <stdio.h>
 #include "../cpu.h"
+#include "../mmu/mmu.h"
 
+void LDMEM(void) {
+    WORD32* Memory = CpuCtx->Memory;
+    union {
+        BYTE As8;
+        struct {
+            BYTE As4_0 : 4;
+            BYTE As4_1 : 4;
+        };
+    }Byte8;
+    Byte8.As8 = MmuRead1();
+    
+    WORD32* Destination = &Rs->GPRs[Byte8.As4_0];
+    *Destination = *((BYTE*)Memory + Rs->GPRs[Byte8.As4_1]);
+    
+    return;
+} //LDMEM 40-16 (R:04,04=__DEST) (R:04,04=ADDRES) : Loads memory into a register
+
+void STMEM(void) {
+    WORD32* Memory = CpuCtx->Memory;
+    union {
+        BYTE As8;
+        struct {
+            BYTE As4_0 : 4;
+            BYTE As4_1 : 4;
+        };
+    }Byte8;
+    Byte8.As8 = MmuRead1();
+    
+    WORD32* Destination = (WORD32*)((BYTE*)Memory +
+        Rs->GPRs[Byte8.As4_1]);
+    *Destination = Rs->GPRs[Byte8.As4_0];
+    
+    return;
+} // STMEM 41-16 (R:04,04=__DEST) (R:04,04=SOURCE) : Stores memory from register
